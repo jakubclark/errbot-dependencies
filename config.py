aa67f92c492db297abf8887d2baa5978bfaeda82
Kuba Clark <jakub.clark@protonmail.com>
2019-12-05 20:15:28 +0100

    Add config.py

---
+++ config.py
@@ -0,0 +1,11 @@
+import logging
+
+BOT_EXTRA_PLUGIN_DIR = './dependent_plugins'
+BACKEND = 'Text'
+BOT_DATA_DIR = './data'
+BOT_ADMINS = ('#gbin@localhost',)
+BOT_LOG_FILE = BOT_DATA_DIR + '/err.log'
+BOT_LOG_LEVEL = logging.INFO
+BOT_PREFIX_OPTIONAL_ON_CHAT = True
+BOT_PREFIX = ''
+CORE_PLUGINS = ('Help', )
