e424afbb3620e0e7f070e032ef9fac9a06c1a529
Kuba Clark <jakub.clark@protonmail.com>
2019-12-05 20:21:37 +0100

    fix BOT_ADMINS

--- config.py
+++ config.py
@@ -3,7 +3,7 @@ import logging
 BOT_EXTRA_PLUGIN_DIR = './dependent_plugins'
 BACKEND = 'Text'
 BOT_DATA_DIR = './data'
-BOT_ADMINS = ('#gbin@localhost',)
+BOT_ADMINS = ('@foo',)
 BOT_LOG_FILE = BOT_DATA_DIR + '/err.log'
 BOT_LOG_LEVEL = logging.INFO
 BOT_PREFIX_OPTIONAL_ON_CHAT = True
